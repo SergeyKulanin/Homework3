63fa32bc98fc8eee291d1d2fac153bf3edb47131
Sergey Kulanin <sjulanin1981@gmail.com>
2023-02-24 18:44:10 +0300

    проверка палиндрома

---
+++ Ex19/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+Console.WriteLine("Введите пятизначное число: ");
+string number = int.Parse(Console.ReadLine());
+int lenght = number.Length;
+
+if (lenght == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - "Это палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - "Это не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"{number} - не является пятизначным");
+}
